8d5ef2444ee584162a95626e20b0fd3c7bc0e9da
Xavero <evandromelos@hotmail.com>
2018-08-09 23:03:55 -0300

    Section 3, lecture 34

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>I'm React App</h1>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <h1>I'm React App</h1>
+                <p>This is really working!</p>
+                <Person/>
+            </div>
+        );
+    }
 }
 
 export default App;
