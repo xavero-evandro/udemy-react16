4b0ae2596161c36b77d345732c9ee60e567c979d
Xavero <evandromelos@hotmail.com>
2018-08-16 23:02:48 -0300

    Section 3, lecture 47

--- src/App.js
+++ src/App.js
@@ -3,12 +3,65 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+    state = {
+        persons: [
+            {name: 'Xavero', age: 30},
+            {name: 'Lalalele', age: 20},
+            {name: 'Uhul', age: 10}
+        ]
+    };
+
+    switchNameHandler = (newName) => {
+        this.setState({
+            persons: [
+                {name: newName, age: 30},
+                {name: 'Lalalele', age: 20},
+                {name: 'Uhul', age: 12}
+            ]
+        })
+    };
+
+    nameChangedHandler = (event) => {
+        this.setState({
+            persons: [
+                {name: 'Xavero', age: 30},
+                {name: event.target.value, age: 20},
+                {name: 'Uhul', age: 12}
+            ]
+        })
+    };
+
     render() {
+        const style = {
+            backgroundColor: 'white',
+            font: 'inherit',
+            border: '1px solid blue',
+            padding: '9px',
+            cursor: 'pointer'
+        };
         return (
             <div className="App">
                 <h1>I'm React App</h1>
                 <p>This is really working!</p>
-                <Person/>
+
+                <button
+                    style={style}
+                    onClick={() => this.switchNameHandler('Bolor')}/*INEFFICIENT BINDING*/>
+                    Switch Name
+                </button>
+                <Person
+                    name={this.state.persons[0].name}
+                    age={this.state.persons[0].age}/>
+                <Person
+                    name={this.state.persons[1].name}
+                    age={this.state.persons[1].age}
+                    click={this.switchNameHandler.bind(this, 'Dalee')}// EFFICIENT BINDING
+                    changed={this.nameChangedHandler}>
+                    Hobbies HAHAHA
+                </Person>
+                <Person
+                    name={this.state.persons[2].name}
+                    age={this.state.persons[2].age}/>
             </div>
         );
     }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import './Person.css';
 
-const person = () => {
-    return <p>I'm a Person</p>
+const person = (props) => {
+    return (
+        <div className="Person">
+            <p onClick={props.click}>I'm {props.name} and I'm {props.age} years old</p>
+            <p>{props.children}</p>
+            <input type="text" value={props.name} onChange={props.changed}/>
+        </div>
+    )
 };
 
 export default person;
